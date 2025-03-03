a7a9464c15b57a3c73725494d999619aaf106adb
Rebellion <avinashee0012@gmail.com>
2025-03-03 11:33:53 +0530

    Focusing on project instead of all the setup. Switcing to springboot instead.

--- src/main/webapp/js/script.js
+++
@@ -1,8 +0,0 @@
-function fetchMessage() {
-    fetch('/UnitConverterWebApp/hello')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("message").innerText = data;
-        })
-        .catch(error => console.error('Error:', error));
-}
